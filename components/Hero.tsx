8ab5b5c7b1c594a919898bcd87edb26bbcd95fc6
nbarzon <01158672@pw.edu.pl>
2025-03-08 23:33:30 +0100

    Intro page v1.0

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Spotlight } from "./ui/spotlight-new";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { MagicButton } from "./ui/MagicButton";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
         <Spotlight />
-        <div className="container">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-center font-serif font-bold leading-tight ">Exploring the Significance of Mental Health <br></br>Through Data Science</h1>
-            <p className="mt-4 text-lg sm:text-xl lg:text-2xl text-center text-gray-300 font-sans">
-                Discover the latest trends and insights in global health statistics. Filter by country, disease, and more to uncover meaningful patterns and gain a deeper understanding of the world's healthcare landscape.
-            </p>
+
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl text-center font-serif font-bold leading-tight ">
+                Exploring the Significance of Mental Health <br></br>Through Data Science
+            </h1>
+            <TextGenerateEffect 
+                words="Discover the latest trends and insights in global health statistics. Filter by country, disease, and more to uncover meaningful patterns and gain a deeper understanding of the world's healthcare landscape."
+                className="mt-4 text-lg sm:text-l lg:text-xl text-center text-gray-300 font-sans" 
+                duration={0.5}
+            />
         </div>
+        <a className="flex justify-center mt-8" href="#explore"> 
+                <MagicButton />
+            </a>
       </div>
-    </div>
+
   );
 };
 
---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MagicButton = () => {
+  return (
+  <button className="shadow-[inset_0_0_0_2px_#616467] px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white transition duration-200">
+    Start
+  </button>
+  )
+}
